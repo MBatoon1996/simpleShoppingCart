dfe9b57a8194624343d7abd682fbb358f88f6001
Marques <MBatoon1996@gmail.com>
2021-01-29 11:39:18 -1000

    added total price and changed width settings

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ var getSubtotal = function(){
         price = parseFloat(price.substring(1));
         var qty = parseFloat($(ele).find('input.qtyInput').val());
         var subtotal = price * qty;
+        subtotal = "$"+subtotal.toFixed(2);
         $(ele).children('.subtotal').html(subtotal);
     });
 }
 
+var getTotal = function(){
+    var total = 0;
+    $('tbody tr').each(function(i, ele){
+        var subtotal = $(ele).children('.subtotal').text();
+        subtotal = parseFloat(subtotal.substring(1));
+        total+=subtotal;
+    });
+    $('#total').html("$"+total);
+}
+
 $(document).ready(function(){
     getSubtotal();
+    getTotal();
 });
 
 $('.btn.cancel').on('click', function(event){
